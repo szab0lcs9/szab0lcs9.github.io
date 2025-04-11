4cd05641474a707730273a99d491b957772e5d10
Szabolcs Veres <szabolcs.veres09@gmail.com>
2025-04-11 09:59:46 +0200

    added background, navbar, footer to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Background from './components/Background'
 
 function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="relative min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-gray-300 flex flex-col items-center overflow-hidden">
+        <Background />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   )
 }
