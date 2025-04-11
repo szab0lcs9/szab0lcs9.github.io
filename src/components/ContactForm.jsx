41da840fcf1be896b0ea32043f9473f4ddb222f9
Szabolcs Veres <szabolcs.veres09@gmail.com>
2025-04-11 10:01:19 +0200

    added funcionality to the submit button

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,14 @@
 const ContactForm = () => {
+
+    const submitForm = () => {
+        console.log("Form submitted");
+    }
+
     return (
         <div className="w-full md:w-200 bg-0 p-10 z-100">
             <h1 className="text-3xl font-bold mb-4">Contact Me</h1>
             <p className="mb-4">Feel free to reach out to me via email or connect with me on LinkedIn!</p>
-            <form className="bg-white p-6 rounded shadow-md">
+            <form className="bg-0">
                 <div className="mb-4">
                     <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
                     <input type="text" id="name" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500" />
@@ -16,7 +21,9 @@ const ContactForm = () => {
                     <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
                     <textarea id="message" rows="4" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500"></textarea>
                 </div>
-                <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Send Message</button>
+                <button type="submit" 
+                        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600" 
+                        onClick={() => submitForm()}>Send Message</button>
             </form>
         </div>
     );
