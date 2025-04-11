932515377ef5d938d8709c474e8ddd32d260a359
Szabolcs Veres <szabolcs.veres09@gmail.com>
2025-04-11 10:02:19 +0200

    remove code duplications

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,8 @@
-import Background from "../components/Background";
-import Navbar from "../components/Navbar";
 import ContactForm from "../components/ContactForm";
-import Footer from "../components/Footer";
 
-export default function Contact() {
+const Contact = () => {
     return (
-        <div className="relative min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-gray-300 flex flex-col items-center overflow-hidden">
-            <Background />
-            <Navbar />
-            <ContactForm />
-            <Footer />
-        </div>
-    );
-    
+        <ContactForm />
+    )
 }
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,8 @@
-import Background from "../components/Background";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
-export default function Home() {
+const Home = () => {
     return (
-        <div className="relative min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-gray-300 flex flex-col items-center overflow-hidden">
-            <Background />
-            <Navbar />
-            <Footer />
-        </div>
-    );
-}
+        <>  
+        </>
+    )
+};
+
+export default Home;
