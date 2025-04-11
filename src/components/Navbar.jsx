4b4849a2428d5aa7d74c1da85735c508cb358710
Szabolcs Veres <szabolcs.veres09@gmail.com>
2025-04-11 10:00:16 +0200

    modified colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,21 +12,21 @@ const Navbar = () => {
         <header className="w-full py-6 px-10 flex justify-between items-center bg-gray-900 bg-opacity-80 backdrop-blur-lg shadow-lg relative z-10">
             <motion.h1 
                 className="text-2xl md:text-4xl font-bold tracking-wide text-blue-400" 
-                initial={{ opacity: 0, x: -20 }} 
+                initial={{ opacity: 0, x: -60 }} 
                 animate={{ opacity: 1, x: 0 }} 
                 transition={{ duration: 0.6 }}
-            ><a href="#home">Veres Szabolcs</a>
+            ><Link to="/">Veres Szabolcs</Link>
             </motion.h1>
             <nav className='hidden md:flex'>
                 <ul className="flex gap-8 text-lg font-medium text-blue-300">
-                    <motion.li whileHover={{ scale: 1.1 }}><a href="#about" className="hover:text-blue-400 transition">About</a></motion.li>
-                    <motion.li whileHover={{ scale: 1.1 }}><a href="#projects" className="hover:text-blue-400 transition">Projects</a></motion.li>
+                    <motion.li whileHover={{ scale: 1.1 }}><Link to="/about" className="hover:text-blue-400 transition">About</Link></motion.li>
+                    <motion.li whileHover={{ scale: 1.1 }}><Link to="/projects" className="hover:text-blue-400 transition">Projects</Link></motion.li>
                     <motion.li whileHover={{ scale: 1.1 }}><Link to="/contact" className="hover:text-blue-400 transition">Contact</Link></motion.li>
                 </ul>
             </nav>
 
             <div className="md:hidden z-20">
-                <button onClick={() => setMenuOpen(!menuOpen)} className="text-indigo-300 hover:text-indigo-400 transition">
+                <button onClick={() => setMenuOpen(!menuOpen)} className="text-blue-300 hover:text-blue-400 transition">
                 {menuOpen ? <X size={32} /> : <Menu size={32} />}
                 </button>
             </div>
@@ -34,14 +34,14 @@ const Navbar = () => {
 
             {menuOpen && (
                 <motion.div
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                className="absolute top-full left-0 w-full bg-gray-900 bg-opacity-95 backdrop-blur-md text-center py-6 flex flex-col gap-6 text-lg text-indigo-300 md:hidden"
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    className="absolute top-full left-0 w-full bg-gray-900 bg-opacity-95 backdrop-blur-md text-center py-6 flex flex-col gap-6 text-lg text-blue-300 md:hidden"
                 >
-                <a href="#about" onClick={() => setMenuOpen(false)} className="hover:text-indigo-400 transition">About</a>
-                <a href="#projects" onClick={() => setMenuOpen(false)} className="hover:text-indigo-400 transition">Projects</a>
-                <a href="#contact" onClick={() => setMenuOpen(false)} className="hover:text-indigo-400 transition">Contact</a>
+                    <Link to="/about" onClick={() => setMenuOpen(false)} className="hover:text-blue-400 transition">About</Link>
+                    <Link to="/projects" onClick={() => setMenuOpen(false)} className="hover:text-blue-400 transition">Projects</Link>
+                    <Link to="/contact" onClick={() => setMenuOpen(false)} className="hover:text-blue-400 transition">Contact</Link>
                 </motion.div>
             )}
         </header>
