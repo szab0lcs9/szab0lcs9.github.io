7787f1c3e066d52a616f68c820ab8d3a5bfefdb4
Szabolcs Veres <szabolcs.veres09@gmail.com>
2025-04-19 23:57:42 +0200

    style fixing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import ResumeTimeline from "../components/ResumeTimeline";
 
 const About = () => {
   return (
-    <div className="px-4 sm:px-6 md:px-8 py-12 max-w-3xl mx-auto">
+    <div className="px-4 md:px-8 py-12 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-8">About Me</h1>
       <ResumeTimeline />
     </div>
